abc562ec93213c292a1570f7c223c28341501f18
ismayilhsnv <tu7u02d2s@code.edu.az>
2023-11-06 13:34:46 +0400

    added

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,30 @@ body.addEventListener('mousemove',(e)=>{
     let y=Math.floor(e.clientY/100)
     image_right.style.transform=`translate(${-x}px,${-y}px)`
 })
+
+
+
+
+const btnleft=document.querySelector(".fa-angle-right")
+const btnright=document.querySelector(".fa-angle-left")
+const image=document.querySelector("#image_right")
+btnleft.onclick=()=>{
+    if(image.src==='https://htmldemo.net/pronia/pronia/assets/images/slider/inner-img/1-2-524x617.png'){
+        image.src='https://htmldemo.net/pronia/pronia/assets/images/slider/inner-img/1-1-524x617.png'
+        image.style.transition='0.5s'
+    }else {
+        image.src='https://htmldemo.net/pronia/pronia/assets/images/slider/inner-img/1-2-524x617.png'
+        image.style.transition='0.5s'
+    }
+
+    
+}
+btnright.onclick=()=>{
+    if(image.src==='https://htmldemo.net/pronia/pronia/assets/images/slider/inner-img/1-1-524x617.png'){
+        image.src='https://htmldemo.net/pronia/pronia/assets/images/slider/inner-img/1-2-524x617.png'
+        image.style.transition='0.5s'
+    }else {
+        image.src='https://htmldemo.net/pronia/pronia/assets/images/slider/inner-img/1-1-524x617.png'
+        image.style.transition='0.5s'
+    }
+}
